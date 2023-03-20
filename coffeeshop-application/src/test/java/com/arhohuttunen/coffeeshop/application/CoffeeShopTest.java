11ffc970d10b0e8c86a2280353de0a397f243515
Arho Huttunen <arho.huttunen@iki.fi>
2023-03-20 15:11:54 +0200

    Move some business logic testing into a more focused unit test

--- coffeeshop-application/src/test/java/com/arhohuttunen/coffeeshop/application/CoffeeShopTest.java
+++ coffeeshop-application/src/test/java/com/arhohuttunen/coffeeshop/application/CoffeeShopTest.java
@@ -2,23 +2,18 @@ package com.arhohuttunen.coffeeshop.application;
 
 import com.arhohuttunen.coffeeshop.application.order.Order;
 import com.arhohuttunen.coffeeshop.application.order.OrderItem;
-import com.arhohuttunen.coffeeshop.application.out.stubs.InMemoryOrders;
-import com.arhohuttunen.coffeeshop.application.out.stubs.InMemoryPayments;
 import com.arhohuttunen.coffeeshop.application.out.Orders;
 import com.arhohuttunen.coffeeshop.application.out.Payments;
+import com.arhohuttunen.coffeeshop.application.out.stubs.InMemoryOrders;
+import com.arhohuttunen.coffeeshop.application.out.stubs.InMemoryPayments;
 import com.arhohuttunen.coffeeshop.shared.Drink;
 import com.arhohuttunen.coffeeshop.shared.Location;
 import com.arhohuttunen.coffeeshop.shared.Milk;
 import com.arhohuttunen.coffeeshop.shared.Size;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
-import java.math.BigDecimal;
 import java.util.List;
-import java.util.stream.Stream;
 
 import static com.arhohuttunen.coffeeshop.application.order.OrderTestFactory.aPaidOrder;
 import static com.arhohuttunen.coffeeshop.application.order.OrderTestFactory.aReadyOrder;
@@ -28,20 +23,11 @@ import static com.arhohuttunen.coffeeshop.application.payment.CreditCardTestFact
 import static com.arhohuttunen.coffeeshop.application.payment.PaymentTestFactory.aPaymentForOrder;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class CoffeeShopTest {
     private Orders orders;
     private Payments payments;
     private CoffeeShop coffeeShop;
-
-    private static Stream<Arguments> drinkCosts() {
-        return Stream.of(
-                arguments(1, Size.SMALL, BigDecimal.valueOf(4.0)),
-                arguments(1, Size.LARGE, BigDecimal.valueOf(5.0)),
-                arguments(2, Size.SMALL, BigDecimal.valueOf(8.0))
-        );
-    }
 
     @BeforeEach
     void setup() {
@@ -55,28 +41,6 @@ class CoffeeShopTest {
         var order = coffeeShop.createOrder(anOrder());
 
         assertThat(order.isUnpaid()).isTrue();
-    }
-
-    @ParameterizedTest(name = "{0} drinks of size {1} cost {2}")
-    @MethodSource("drinkCosts")
-    void creatingOrderReturnsCost(int quantity, Size size, BigDecimal expectedCost) {
-        var order = coffeeShop.createOrder(
-                new Order(Location.TAKE_AWAY, List.of(new OrderItem(Drink.LATTE, quantity, Milk.WHOLE, size)))
-        );
-
-        assertThat(order.getCost()).isEqualTo(expectedCost);
-    }
-
-    @Test
-    void creatingOrderWithMultipleItemsReturnsCost() {
-        var order = coffeeShop.createOrder(
-                new Order(Location.TAKE_AWAY, List.of(
-                        new OrderItem(Drink.LATTE, 1, Milk.SKIMMED, Size.LARGE),
-                        new OrderItem(Drink.ESPRESSO, 1, Milk.SOY, Size.SMALL)
-                ))
-        );
-
-        assertThat(order.getCost()).isEqualTo(BigDecimal.valueOf(9.0));
     }
 
     @Test
---
+++ coffeeshop-application/src/test/java/com/arhohuttunen/coffeeshop/application/order/OrderTest.java
@@ -0,0 +1,48 @@
+package com.arhohuttunen.coffeeshop.application.order;
+
+import com.arhohuttunen.coffeeshop.shared.Drink;
+import com.arhohuttunen.coffeeshop.shared.Location;
+import com.arhohuttunen.coffeeshop.shared.Milk;
+import com.arhohuttunen.coffeeshop.shared.Size;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class OrderTest {
+
+    private static Stream<Arguments> drinkCosts() {
+        return Stream.of(
+                arguments(1, Size.SMALL, BigDecimal.valueOf(4.0)),
+                arguments(1, Size.LARGE, BigDecimal.valueOf(5.0)),
+                arguments(2, Size.SMALL, BigDecimal.valueOf(8.0))
+        );
+    }
+
+    @ParameterizedTest(name = "{0} drinks of size {1} cost {2}")
+    @MethodSource("drinkCosts")
+    void creatingOrderReturnsCost(int quantity, Size size, BigDecimal expectedCost) {
+        var order = new Order(Location.TAKE_AWAY, List.of(
+                new OrderItem(Drink.LATTE, quantity, Milk.WHOLE, size)
+        ));
+
+        assertThat(order.getCost()).isEqualTo(expectedCost);
+    }
+
+    @Test
+    void creatingOrderWithMultipleItemsReturnsCost() {
+        var order = new Order(Location.TAKE_AWAY, List.of(
+                new OrderItem(Drink.LATTE, 1, Milk.SKIMMED, Size.LARGE),
+                new OrderItem(Drink.ESPRESSO, 1, Milk.SOY, Size.SMALL)
+        ));
+
+        assertThat(order.getCost()).isEqualTo(BigDecimal.valueOf(9.0));
+    }
+}
