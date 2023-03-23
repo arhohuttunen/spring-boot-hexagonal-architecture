3ce3a167e7c94aaca391726b136019410b197ae1
Arho Huttunen <arho.huttunen@iki.fi>
2023-03-23 12:50:56 +0200

    Fix formatting

--- coffeeshop-application/src/main/java/com/arhohuttunen/coffeeshop/application/payment/CreditCard.java
+++ coffeeshop-application/src/main/java/com/arhohuttunen/coffeeshop/application/payment/CreditCard.java
@@ -3,4 +3,4 @@ package com.arhohuttunen.coffeeshop.application.payment;
 import java.time.Month;
 import java.time.Year;
 
-public record CreditCard(String cardHolderName, String cardNumber, Month expiryMonth, Year expiryYear) {}
+public record CreditCard(String cardHolderName, String cardNumber, Month expiryMonth, Year expiryYear) { }
--- coffeeshop-application/src/main/java/com/arhohuttunen/coffeeshop/application/payment/Receipt.java
+++ coffeeshop-application/src/main/java/com/arhohuttunen/coffeeshop/application/payment/Receipt.java
@@ -3,4 +3,4 @@ package com.arhohuttunen.coffeeshop.application.payment;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record Receipt(BigDecimal amount, LocalDate paid) {}
+public record Receipt(BigDecimal amount, LocalDate paid) { }
